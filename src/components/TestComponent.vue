5e27b224205596fffbb378aa245fc19669a7d26a
nik <nik.abr96@gmail.com>
2020-11-06 11:34:50 +0200

    first commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Test Text</p>
+    <h1>{{ testText }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  props: {
+    testText: String,
+  },
+};
+</script>
